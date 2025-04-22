e84d73d58edf992f20448adf8bf68ea9ec463710
wnstlrWkd123 <prkjnsk@naver.com>
2025-04-22 17:38:33 +0900

    test: 커밋 테스트
    커밋테스트용입니다.

--- 15jijo/Program.cs
+++ 15jijo/Program.cs
@@ -1,10 +1,9 @@
-﻿namespace _15jijo
+namespace _15jijo;
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        Console.WriteLine("커밋 테스트");
     }
 }

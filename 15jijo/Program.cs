ed141ff7f0ec072bd22ad33da401427a3851009f
wnstlrWkd123 <prkjnsk@naver.com>
2025-04-22 22:16:22 +0900

    docs: 클래스 찢기
    클래스를 찢었습니다.

--- 15jijo/Program.cs
+++ 15jijo/Program.cs
@@ -1,48 +1,9 @@
-namespace _15jijo;
-
 internal class Program
 {
     static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            GameManager gameManager = new GameManager();
-            gameManager.Init();
-            gameManager.GameStart();
-        }
+        GameManager gameManager = new GameManager();
+        gameManager.Init();
+        gameManager.GameStart();
     }
-}
-
-enum Jobs
-{
-    전사,
-    궁수,
-    마법사
-}
-
-internal class Skill
-{
-    private int Job { get; }
-    private int Grade { get; }
-    private string Name { get; }
-    private string Description { get; }
-    private float Damage { get; }
-    private int ConsumedMp { get; }
-
-    private Skill(Jobs jobs, int grade, string name, string description, float damage, int consumedMp)
-    {
-        Job = (int)jobs;
-        Grade = grade;
-        Name = name;
-        Description = description;
-        Damage = damage;
-        ConsumedMp = consumedMp;
-    }
-
-    public List<Skill> list = new List<Skill>()
-    {
-        new Skill(Jobs.전사, 0, "알파 스트라이크", "공격력 * 2 로 하나의 적을 공격합니다.", 1 * 2, 10),//player.BasicAttackPower * 2
-        new Skill(Jobs.궁수, 0, "파이어 샷", "공격력 * 2 로 하나의 적을 공격합니다.", 1 * 2, 10),
-        new Skill(Jobs.마법사, 0, "에너지 볼", "공격력 * 2 로 하나의 적을 공격합니다.", 1 * 2, 10)
-    };
 }
---
+++ 15jijo/junsik/EnumJobs.cs
@@ -0,0 +1,6 @@
+public enum Jobs
+{
+    전사,
+    궁수,
+    마법사
+}

825e486cefa32d8ee0dd8f2e35b49ce9bf187135
OHSUHO <suho.oh1213@gmail.com>
2025-04-24 13:08:39 +0900

    feat: 퀘스트 기능 추가
    퀘스트 기능 추가

--- 15jijo/Program.cs
+++ 15jijo/Program.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Runtime.InteropServices;
 using System.Xml.Linq;
 namespace _15jijo
 {
@@ -9,8 +10,10 @@ namespace _15jijo
         static void Main(string[] args)
         {
             GameManager gameManager = new GameManager();
-            gameManager.Init();
-            gameManager.GameStart();
+            //gameManager.Init();
+            QuestController qc = new QuestController();
+            qc.DisplayQuest();
+            
         }
 
         class Player
@@ -41,44 +44,59 @@ namespace _15jijo
 
         class Item
         {
-            private Player? _player;
-            public void GetStatFromItem()
-            {
-                if (_player != null)
-                    _player.ATTACK += 10;
+            string name;
+            public string NAME{get;set;}
+            public Item(){
+                name = "포션";
+                }
 
-            }
+        }
+
+        enum questType
+        {
+            killMonsterQuest = 0,
+            overStatQuest,
+            
+        }
+
+        enum questRequireStatName
+        {
+            공격력 = 0,
+            방어력,
+            레벨
 
         }
         abstract class Quest
         {
-            private string? questName; // 퀘스트 이름
-            protected string? QUESTNAME { get; set; }
-            private string? questDesc; // 퀘스트 설명
-            protected string? QUESTDESC { get; set; }
-            private int reward_Gold; // 보상 골드
-            protected int REWARD_GOLD { get; set; }
-            private float reward_exp; // 보상 경험치
-            protected float REWARD_EXP { get; set; }
-            private List<Item>? Reward_Items; // 보상 아이템 리스트
-            protected List<Item>? REWARD_ITEMS { get; set; }
-            private bool isClear = false; // 이미 클리어 한 퀘스트 인가?
-            public bool ISCLEAR { get; set; }
-            private bool isReceive = false; // 이미 받은 퀘스트 인가?
-            public bool ISRECEIVE { get; set; }
-            public string? questCore;
+            protected string? QuestName { get; set; } // 퀘스트 이름
+            protected string? QuestDesc { get; set; } // 퀘스트 설명
+            protected int Reward_Gold { get; set; } // 보상 골드
+            protected float Reward_Exp { get; set; } // 보상 경험치
+            protected List<Item>? Reward_Items { get; set; } // 보상 아이템
+            public bool IsClear { get; set; } = false;
+            public bool IsReceive { get; set; } = false;
+            protected string? questCore;
 
             public abstract void CompleteQuest(); // 퀘스트 유형 별 퀘스트 완료 로직 구현을 오버라이딩 할 추상 메서드
             public abstract void makeQuestCore(); // 퀘스트 유형 별 퀘스트 핵심 출력 로직 구현을 오버라이딩 할 추상 메서드 
 
+            public Quest(string name, string desc, int gold, float exp,List<Item>items)
+            {
+                this.QuestName = name;
+                this.QuestDesc = desc;
+                this.Reward_Gold = gold;
+                this.Reward_Exp = exp;
+                this.Reward_Items = items;
+            }
+
             public virtual void ShowQuestName()
             {
                 string? currentQuestState = "";
-                if (ISCLEAR)
+                if (IsClear)
                 {
                     currentQuestState += "(완료)";
                 }
-                else if (ISRECEIVE)
+                else if (IsReceive)
                 {
                     currentQuestState += "(진행중)";
                 }
@@ -87,21 +105,21 @@ namespace _15jijo
                     // 받은 상태가 아니면 퀘스트 이름 옆에 추가할 string이 있을까?
                 }
 
-                Print.print($"{QUESTNAME} {currentQuestState}\n");
+                Print.print($"{QuestName} {currentQuestState}\n");
             }
 
             public virtual void ShowQuest()
             {
                 ShowQuestName();
-                Print.print($"{QUESTDESC}\n");
+                Print.print($"{QuestDesc}\n");
                 Print.print(questCore);// 퀘스트 핵심 내용
                 Print.print("-보상-");
-                int rewardItemCount = REWARD_ITEMS == null ? 0 : REWARD_ITEMS.Count;
+                int rewardItemCount = Reward_Items == null ? 0 : Reward_Items.Count;
                 if (rewardItemCount > 0)
                 {
                     //만약 보상 아이템이 존재하면 출력!
                 }
-                Print.print($"{REWARD_GOLD} G \n{REWARD_EXP} exp\n");
+                Print.print($"{Reward_Gold} G \n{Reward_Exp} exp\n");
             }
         }
 
@@ -110,20 +128,20 @@ namespace _15jijo
             private int killCount = 0;
             private int goalCount = 0;
             private Monster targetMonster;
-            public KillMonsterQuest(Monster targetMonster)
-            {
+            public KillMonsterQuest(string name, string desc, int gold, float exp, List<Item> items,Monster targetMonster,int goal):base(name, desc,gold,exp,items)
+            {   
                 this.targetMonster = targetMonster;
-                goalCount = new Random().Next(3, 10); // 랜덤으로 몇 마리의 몬스터를 잡을 지 퀘스트화
-                float bonusReward = 1f + killCount * 0.1f; // 만약 많이 잡는 퀘스트가 걸리면 추가보상
-                REWARD_EXP *= bonusReward; // 추가보상 경험치, 골드
-                REWARD_GOLD = (int)(REWARD_GOLD * bonusReward);
+                this.goalCount = goal;
+                float bonusReward = 1f + goalCount * 0.1f; // 만약 많이 잡는 퀘스트가 걸리면 추가보상
+                Reward_Exp = (Reward_Exp * bonusReward); // 추가보상 경험치, 골드
+                Reward_Gold = (int)(Reward_Gold * bonusReward);
                 makeQuestCore();
 
             }
             public override void makeQuestCore()
             {
-                questCore = $"{targetMonster.GetMonsterName()} {killCount}마리 처치 ({killCount}/{goalCount})\n";
-                
+                questCore = $"{targetMonster.NAME} {goalCount}마리 처치 ({killCount}/{goalCount})\n";
+
             }
 
 
@@ -131,7 +149,7 @@ namespace _15jijo
             {
                 if (killCount >= goalCount)
                 {
-                    ISCLEAR = true;
+                    IsClear = true;
                 }
             }
 
@@ -172,31 +190,136 @@ namespace _15jijo
             }
         }
 
-        
+        class PlayerStatQuest : Quest
+        {
+
+            questRequireStatName statType;
+            float goalStatValue;
+            float currentStatValue;
+
+            public PlayerStatQuest(string name, string desc, int gold, float exp, List<Item> items,questRequireStatName type,int goal) : base(name, desc, gold, exp, items)
+            {
+                //int randomStat = new Random().Next(0, statType.Length);
+                //targetStatName = statType[randomStat];
+                this.goalStatValue = goal;
+                this.statType = type;
+                GetTargetStatValue();
+
+
+            }
+
+
+            public override void makeQuestCore()
+            {
+                questCore = $"{statType}을 {goalStatValue}까지 올리기! {currentStatValue}/{goalStatValue})\n";
+            }
+
+            public override void CompleteQuest()
+            {
+                if (currentStatValue >= goalStatValue)
+                {
+                    IsClear = true;
+                }
+            }
+
+            public void GetTargetStatValue()
+            {
+                if (statType == questRequireStatName.공격력)
+                {
+                    //플레이어의 공격력을 currentStatValue
+                }
+                else if (statType == questRequireStatName.방어력)
+                {
+                    //플레이어의 방어력을 currentStatValue
+                }
+                else if (statType == questRequireStatName.레벨)
+                {
+                    //플레이어의 레벨을 currentStatValue
+                }
+            }
+
+            public void SetGoalStatValue()
+            {
+                // 플레이어의 현재 레벨에 비례해서 증가
+            }
+        }
+
+
 
         class QuestController //퀘스트 씬 출력 및 퀘스트 데이터베이스 세팅 담당 클래스 
         {
-            private Quest[]? questDB;
-            private List<Quest>? playerReceiveQuest;
+            
+            private List<Quest> questList; // 퀘스트들을 담고있는 Quest배열
+            private Monster[]? targetMonsterDB; // 몬스터 DB에서 targetMonster를 랜덤으로 지정
+            private List<Quest>? playerReceiveQuest; // 플레이어가 받은 퀘스트 목록
+            private string[]? statType; // 플레이어의 능력치를 올리는 퀘스트에 사용되는 능력치 타입
             
 
-            static List<Quest> SetQuestDB()
+            public QuestController() // 퀘스트 컨트롤러 생성자 
             {
-                List<Quest> questList = new List<Quest>
+                statType = new string[] { "공격력", "방어력", "레벨" };
+                //targetMonsterDB = ;
+                questList = SetQuestDB();
+                
+            }
+
+            static List<Quest> SetQuestDB()
+            {   /*
+                 퀘스트 타입을 랜덤으로 정하여 퀘스트배열 반환하는 메서드
+                Quest[] questList = new Quest[6];
+                Random rand = new Random();
+                for (int i = 0; i < questList.Length; i++)
+                {   
+                    int r = rand.Next(0,System.Int32.MaxValue);
+                    
+                }
+                
+                return questList;
+                */
+                List<Quest> questList = new List<Quest>()
                 {
-
-
-
+                    new KillMonsterQuest("내일배움캠프를 위협하는 오수호 처치","내일배움캠프를 위협하는 오수호를 처리해주시게",100,100f,new List<Item>(){new Item()},new Slime(),5),
+                    new PlayerStatQuest("어쩌라고 공격력 올려와","퀘스트 출력하는 공간인데 어쩌라고",150,200f,new List<Item>(){new Item()},questRequireStatName.공격력,5)
                 };
                 return questList;
             }
 
-            public void ClearQuest()
+            public void DisplayQuest()
+            {
+                foreach(Quest q in questList)
+                {
+                    q.ShowQuest();
+                    q.ShowQuestName();
+                }
+
+
+            }
+            /*
+             퀘스트 type에 따라 랜덤생성하는 메서드 
+            Quest GenerateQuest(questType type)
+            {   Random rand = new Random();
+                switch (type)
+                {
+                    case questType.killMonsterQuest:
+                        int i = rand.Next(0, targetMonsterDB.Length);
+                        new KillMonsterQuest($"내일배움캠프를 위협하는 {targetMonsterDB[i].NAME} 처치", );
+                        break;
+
+
+
+                }
+
+
+                return Quest 
+
+            }
+            */
+            public void CompleteQuest()
             {
                 //퀘스트 클리어 로직
-            } 
-            
-           
+            }
+
+
 
         }
 
@@ -204,9 +327,9 @@ namespace _15jijo
         {
             public static void print(string str)
             {
-                int age = 1;
+               
                 Console.WriteLine(str);
-                Console.WriteLine($"{age}");
+                
             }
         }
 
@@ -214,7 +337,7 @@ namespace _15jijo
         abstract class Monster
         {
             string? name;
-            protected string? NAME { get; set; }
+            public string? NAME { get; set; }
             int level;
             protected int LEVEL { get; set; }
 
@@ -229,13 +352,10 @@ namespace _15jijo
 
         class Slime : Monster
         {
-            Slime()
+            public Slime()
             {
                 NAME = "slime";
             }
-
-
-
 
         }
 
